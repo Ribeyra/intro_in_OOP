d3f59c5fb6e2e02891d22ceb9c6e331450200d1c
ribeyra <ribeyra@gmail.com>
2024-03-01 18:14:12 +0300

    update CustomList

--- intro_in_oop/4 (CustomList).py
+++ intro_in_oop/4 (CustomList).py
@@ -3,11 +3,11 @@
 
 class CustomList:
     def __init__(self, incoming_values={}):
-        self.last_index = 0
+        self.last_index = -1
         self.values = {}
         for value in incoming_values:
-            self.values[self.last_index] = value
             self.last_index += 1
+            self.values[self.last_index] = value
 
     def append(self, *value):
         if len(value) > 1:
@@ -22,23 +22,23 @@ class CustomList:
     def __getitem__(self, index):
         if not isinstance(index, int):
             raise TypeError('list indices must be integers or slices')
-        elif index < 0 and index < -(self.last_index + 1):
+        elif index < -(self.last_index + 1):
             raise IndexError('list index out of range')
         elif index > self.last_index:
             raise IndexError('list index out of range')
         if index < 0:
-            index = (self.last_index + 1) - abs(index)
+            index = self.last_index + 1 + index
         return self.values.get(index)
 
     def __setitem__(self, index, value):
         if not isinstance(index, int):
             raise TypeError('list indices must be integers or slices')
-        elif index < 0 and index < -(self.last_index + 1):
+        elif index < -(self.last_index + 1):
             raise IndexError('list index out of range')
         elif index > self.last_index:
             raise IndexError('list index out of range')
         if index < 0:
-            index = (self.last_index + 1) - abs(index)
+            index = self.last_index + 1 + index
         self.values[index] = value
 
     def __repr__(self):
@@ -53,14 +53,18 @@ class CustomList:
 
 
 l = CustomList({'a', 'b'})
+
 print(l)                    # [b, a]
 print(l.view_list())        # [b, a]
+
 CustomList.append(l, 2)
+print(l.last_index)
 # l.append(5, 4) #TypeError: list.append() takes exactly one argument (2 given)
 l.append(5)
 l.append(3)
 l.append(8)
 print(l)                    # [a, b, 2, 5, 3, 8]
 print(l.view_value(1))      # b
-l[1] = 2
+l[1] = 21
+print(l.last_index)
 print(l[-5])                # 5
